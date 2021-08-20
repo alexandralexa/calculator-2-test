5a05dab1a38c1b30d6fdb6fc9e67067f3ff6d93b
alexandraalexa <gubanovasasa@gmail.com>
2021-08-20 18:20:27 +0300

    calculator

---
+++ calculator.js
@@ -0,0 +1,26 @@
+   let a;
+   let b;
+
+   function sum(){
+     alert(+a+ + b);
+   }
+
+   function showMessage2(){
+    a=prompt(`выберете число`);
+    b=prompt(`выберете другое число`);
+  }
+
+   function min(){
+     alert(+a - +b);
+   }
+
+   function multy(){
+     alert(+a * +b)
+   }
+
+   function division(){
+     alert(+a / +b)
+     
+   }
+
+ showMessage2();
